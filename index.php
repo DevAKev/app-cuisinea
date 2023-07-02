f959b029ef983c1e98d414861d0e8d6585229d50
Kevyn Aiche <kevynpro7700@gmail.com>
2023-07-02 18:07:49 +0200

    PHP commit

--- index.php
+++ index.php
@@ -1,13 +1,9 @@
 <?php
-define('_RECIPES_IMG_PATH_', 'uploads/recipes/');
-$recipes = [
-    ['title' => 'Mousse au chocolat', 'description' => 'Mousse quick example text to build on the card title and make up the bulk of the card\'s content.', 'image' => '1-chocolate-au-mousse.jpg'],
-    ['title' => 'Gratin dauphinois', 'description' => 'Gratin quick example text to build on the card title and make up the bulk of the card\'s content.', 'image' => '2-gratin-dauphinois.jpg'],
-    ['title' => 'Salade de chèvre', 'description' => 'Salade quick example text to build on the card title and make up the bulk of the card\'s content.', 'image' => '3-salade.jpg']
-];
 
 require_once('templates/header.php');
+require_once('lib/recipe.php');
 ?>
+
 <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
     <div class="col-10 col-sm-8 col-lg-6">
         <img src="assets/images/logo-cuisinea.jpg" class="d-block mx-lg-auto img-fluid" alt="Logo Cuisinea" width="350" loading="lazy">
@@ -23,18 +19,9 @@ require_once('templates/header.php');
 
 <div class="row">
 
-    <?php foreach ($recipes as $key => $recipe) { ?>
-        <div class="col-md-4">
-            <div class="card">
-                <img src="<?= _RECIPES_IMG_PATH_ . $recipe['image']; ?>" class="card-img-top" alt="<?= $recipe['title']; ?>">
-                <div class="card-body">
-                    <h5 class="card-title"><?= $recipe['title']; ?></h5>
-                    <p class="card-text"><?= $recipe['description']; ?></p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
-    <?php } ?>
+    <?php foreach ($recipes as $key => $recipe) {
+        include('templates/recipe_partial.php');
+    } ?>
 
 </div>
 <?php
--- templates/header.php
+++ templates/header.php
@@ -1,3 +1,7 @@
+<?php
+require_once('lib/config.php');
+?>
+
 <!DOCTYPE html>
 <html lang="fr" data-bs-theme="dark">
 
