f26d7282394b34f0ea5ab785ca11c9f86d21e616
Kevyn Aiche <kevynpro7700@gmail.com>
2023-07-03 17:24:44 +0200

    BDD

--- templates/header.php
+++ templates/header.php
@@ -1,9 +1,7 @@
 <?php
 require_once('lib/config.php');
 
-$currentpage = basename($_SERVER['PHP_SELF']);
-
-
+$currentPage = basename($_SERVER['SCRIPT_NAME']);
 
 ?>
 
@@ -25,22 +23,19 @@ $currentpage = basename($_SERVER['PHP_SELF']);
         <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
             <div class="col-md-3 mb-2 mb-md-0">
                 <a href="index.php" class="d-inline-flex link-body-emphasis text-decoration-none">
-                    <img src="assets/images/logo-cuisinea-horizontal.jpg" alt="Logo Cuisinea" width="200">
+                    <img src="assets/images/logo-cuisinea-horizontal.jpg" alt="Logo Cuisinea" width="250">
                     </svg>
                 </a>
             </div>
 
             <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0 nav nav-pills">
-                <li class="nav-item"><a href="index.php" class="nav-link <?php if ($currentpage === 'index.php') {
-                                                                                echo 'active';
-                                                                            } ?>">Accueil</a></li>
-                <li class="nav-item"><a href="recettes.php" class="nav-link <?php if ($currentpage === 'recettes.php') {
-                                                                                echo 'active';
-                                                                            } ?>">Nos recettes</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+                <?php foreach ($mainMenu as $key => $value) { ?>
+                    <li class="nav-item"><a href="<?= $key; ?>" class="nav-link <?php if ($currentPage === $key) {
+                                                                                    echo 'active';
+                                                                                } ?>"><?= $value; ?></a></li>
+                <?php } ?>
             </ul>
+
             <div class="col-md-3 text-end">
                 <button type="button" class="btn btn-outline-primary me-2">Login</button>
                 <button type="button" class="btn btn-primary">Sign-up</button>
